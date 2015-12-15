82710cd5ebfffab2f9dfab7d9632384f63cbe349
hx0day <hx0day@hackerdom.ru>
2015-12-15 19:36:08 +0500

    fix(javascript-tasks-9): add test parallel

--- test/test.js
+++ test/test.js
@@ -3,45 +3,58 @@ var assert = require('assert');
 // Ваша супер библиотека
 var flow = require('../lib/flow.js');
 
-
 describe('Check', function () {
-    it('should check that target parallel one error', function () {
+    it('should check that target parallel one error', function (done) {
         var result = [];
 
-        function callback(error, data) {
-            result.push(error);
-        }
 
         function test2(callback) {
-            callback(true, 1);
+            setTimeout(function () {
+                callback(true, 1);
+            }, 0);
         }
 
         function test1(callback) {
-            callback(true, 1);
+            setTimeout(function () {
+                callback(true, 1);
+            }, 0);
         }
 
         var test_function = [test1, test2];
-        flow.parallel(test_function, callback);
-        assert.equal(result.length, 1);
+
+        flow.parallel(test_function, function (error, data) {
+            result.push(error);
+            assert.equal(result.length, 1);
+            done();
+        });
+
+
     });
 
-    it('should check that target parallel', function () {
+    it('should check that target parallel', function (done) {
         var result = [];
 
         function callback(error, data) {
             result = data;
+            assert.deepEqual(result.sort(), [1,2].sort());
+            done();
         }
 
         function test2(callback) {
-            callback(false, 2);
+            setTimeout(function () {
+                callback(false, 1);
+            }, 0);
+
         }
 
         function test1(callback) {
-            callback(false, 1);
+            setTimeout(function () {
+                callback(false, 2);
+            }, 0);
         }
 
         var test_function = [test1, test2];
         flow.parallel(test_function, callback);
-        assert.deepEqual(result, [1, 2]);
+
     });
 });
